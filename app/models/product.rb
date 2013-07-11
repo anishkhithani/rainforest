3af7afa41961f4abc293cbede49507aed5a1e477
Anish Khithani <anish.khithani@gmail.com>
2013-07-11 14:34:44 -0400

    able to respond to json

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents, :sku
+  validates :description, :presence => true
+  validates :name, :presence => true
+  validates :price_in_cents, :presence => true
+  validates :sku, :presence => true
 end
